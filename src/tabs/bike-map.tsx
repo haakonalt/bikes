779eb622f1726abecae11aad3bf651c09206d7c1
Håkon Drolsum Røkenes <haakondr@gmail.com>
2021-06-25 07:49:16 +0200

    increased default zoom level and a better default map center

--- src/tabs/bike-map.tsx
+++ src/tabs/bike-map.tsx
@@ -5,13 +5,14 @@ import type { Station } from '../common/station';
 import type { LatLngExpression } from 'leaflet';
 
 export function BikeMap({ stations }: { stations: Station[] }) {
-  const center: LatLngExpression = [stations[0].lat, stations[1].lon];
+  // probably a good place to travel from..
+  const center: LatLngExpression = [59.91307903384525, 10.745471119880676];
 
   return (
     <>
       <h2>bike map</h2>
 
-      <MapContainer zoom={13} center={center} scrollWheelZoom={true}>
+      <MapContainer zoom={17} center={center} scrollWheelZoom={true}>
         <TileLayer
           attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
